ec34a1e7bd0fd386c0519ef929e6105396a5b423
Nikita Gokhale <81181608+nikitagokhale@users.noreply.github.com>
2022-05-26 22:11:52 +0530

    Update Assignment6.cpp

--- Assignment6.cpp
+++ Assignment6.cpp
@@ -1,244 +1,252 @@
-/* Represent a given graph using adjacency list to perform DFS and BFS. Use the map of the area
- around the college as the graph. Identify the prominent landmarks as nodes and perform DFS
- and BFS on that.*/
-
- #include <iostream>
+//Represent a given graph using adjacency list to perform DFS and BFS. Use the map
+//of the area around the college as the graph. Identify the prominent landmarks as
+//nodes and perform DFS and BFS on that.
+#include<iostream>
+#define SIZE 30
 using namespace std;
 
-class Nodever{
-int destver;
-Nodever *next;
+class Node
+{
+    int data;
+    Node *next;
+
 public:
-Nodever(){
-destver=-1;
-next=NULL;
-}
-Nodever(int destver){
-this->destver=destver;
-next=NULL;
-}
-friend class Graph;
+    Node(int data = 0)
+    {
+        this->data = data;
+        next = nullptr;
+    }
+
+    friend class Graph;
 };
 
-class Stack{
-int top;
-int arrst[10];
+class SLL_Node
+{
+    int data;
+    SLL_Node *next;
+
 public:
-Stack(){
-top=-1;
-for(int i=0;i<10;i++){
-arrst[i]=0;
-}
-}
-void push(int v){
-top++;
-arrst[top]=v;
-}
-int pop(){
-int v=arrst[top];
-top--;
-return v;
-}
-bool isEmpty(){
-if(top==-1){
-return true;
-}
-return false;
-}
-friend class Graph;
+    SLL_Node(int data=0)
+    {
+        this->data = data;
+        next = nullptr;
+    }
+
+    friend class SLL;
 };
 
-class Queue{
-int front,rear;
-int arrqu[10];
-public:
-Queue(){
-front=-1;
-rear=-1;
-for(int i=0;i<10;i++){
-arrqu[i]=0;
-}
-}
-void enqueue(int v){
-if(front==-1){
-front+=1;
-}
-rear+=1;
-arrqu[rear]=v;
-}
+class SLL
+{
+    SLL_Node *head;
+    SLL_Node *traverse;
 
-int dequeue(){
-if(front>rear){
-return -1;
-}
-int v=arrqu[front];
-front+=1;
-return v;
-}
-bool isEmpty(){
-if(front>rear){
-return true;
-}
-return false;
-}
-friend class Graph;
+public:
+    SLL()
+    {
+        head = nullptr;
+        traverse = nullptr;
+    }
+
+    void insert_node(int &ref_data)
+    {
+        if(head == nullptr)
+            head = new SLL_Node(ref_data);
+        else
+        {
+            traverse = head;
+            while(traverse->next!=nullptr)
+                traverse = traverse->next;
+            traverse->next = new SLL_Node(ref_data);
+        }
+    }
+
+    bool find_data(int &ref_data)
+    {
+        traverse = head;
+        while(traverse!=nullptr)
+        {
+            if(traverse->data == ref_data)
+                return true;
+            traverse = traverse->next;
+        }
+        return false;
+    }
+
+    void delete_SLL()
+    {
+        traverse = head;
+        while(traverse!=nullptr)
+        {
+            SLL_Node *temp = traverse;
+            traverse = traverse->next;
+            delete temp;
+        }
+        head = nullptr;
+    }
+
+    int obj_size()
+    {
+        int cnt;
+        traverse = head;
+        while(traverse!=nullptr)
+            cnt++;
+        return cnt;
+    }
+
+    friend class Graph;
 };
 
-class Graph{
-int nver,nedges;
-Nodever **graphpt;
+class Graph
+{
+    Node arr[SIZE];
+    Node *traverse;
+    int no_vertices, no_edges;
+
 public:
-Graph(){
-cout<<"\nEnter the Number of Vertices -----> ";
-cin>>nver;
-cout<<"\nEnter the Number of Edges -----> ";
-cin>>nedges;
-graphpt=new Nodever *[nver];
-for(int i=0;i<nver;i++){
-graphpt[i]=NULL;
-}
-}
-Graph(int nver,int nedges){
-this->nver=nver;
-this->nedges=nedges;
-graphpt=new Nodever *[nver];
-for(int i=0;i<nver;i++){
-graphpt[i]=NULL;
-}
-}
-void insert_edges(){
-for(int i=0;i<nedges;i++){
-int x,y;
-cout<<"\nEnter vertices having edge between them -----> ";
-cin>>x>>y;
-Nodever *t=graphpt[x];
-Nodever *t1=new Nodever(y);
-if(t==NULL){
-graphpt[x]=t1;
-}
-else{
-while(t->next){
-t=t->next;
-}
-t->next=t1;
-}
-}
-}
+    SLL obj, obj2;
+    Graph(int no_vertices = 0, int no_edges = 0)
+    {
+        this->no_vertices = no_vertices;
+        this->no_edges = no_edges;
+        traverse = nullptr;
+    }
 
-void display(){
-   cout<<"\n----------------DISPLAY GRAPH----------------\n"<<endl;
-for(int i=0;i<nver;i++){
-Nodever *p=graphpt[i];
-cout<<" [ "<<i<<" ]";
-while(p){
-cout<<" ---> [ "<<p->destver<<" ] ";
-p=p->next;
-}
-cout<<"\n";
-}
-}
+    void create_graph()
+    {
+        int i, j;
+        cout<<"Enter no. of vertices:- ";
+        cin>>no_vertices;
+        for(i=0;i<no_vertices;i++)
+        {
+            cout<<"\nEnter data for vertex no. "<<i+1<<":- ";
+            cin>>arr[i].data;
+            traverse = &arr[i];
+            cout<<"Enter no. of edges for vertex "<<arr[i].data<<":- ";
+            cin>>no_edges;
+            for(j=0;j<no_edges;j++)
+            {
+                traverse->next = new Node();
+                traverse = traverse->next;
+                cout<<"Enter data for vertex at edge "<<j+1<<":- ";
+                cin>>traverse->data;
+            }
+        }
+    }
 
-void disp_bfs(){
-int v;
-cout<<"\nEnter Starting Vertex for BFS Traversal -----> ";
-cin>>v;
-bool visitedNodes[nver];
-Queue q;
-for(int i=0;i<nver;i++){
-visitedNodes[i]=false;
-}
-Nodever *p=graphpt[v];
-cout<<" [ "<<v<<" ] ";
-visitedNodes[v]=true;
-q.enqueue(v);
-while(!q.isEmpty()){
-int k=q.dequeue();
-if(visitedNodes[k]==false){
-cout<<"---> [ "<<k<<" ] ";
-visitedNodes[k]=true;
-}
-p=graphpt[k];
-while(p){
-if(visitedNodes[p->destver]==false){
-q.enqueue(p->destver);
-}
-p=p->next;
-}
-}
-}
+    void display()
+    {
+        int i, j;
+        for(i=0;i<no_vertices;i++)
+        {
+            cout<<arr[i].data;
+            traverse = &arr[i];
+            while(traverse->next!=nullptr)
+            {
+                traverse = traverse->next;
+                cout<<" ==> "<<traverse->data;
+            }
+            cout<<"\n|\n";
+        }
+    }
 
-void disp_dfs(){
-int v;
-cout<<"\nEnter Starting Vertex for DFS Traversal -----> ";
-cin>>v;
-bool visitedNodes[nver];
-for(int i=0;i<nver;i++){
-visitedNodes[i] = 0;
-}
-Stack s;
-Nodever *p=graphpt[v];
-cout<<" [ "<<v<<" ] ";
-visitedNodes[v]=true;
-s.push(v);
-while(!s.isEmpty()){
-int k=s.pop();
-if(visitedNodes[k]==false){
-cout<<"---> [ "<<k<<" ] ";
-visitedNodes[k]=true;
-}
-p=graphpt[k];
+    void DFS_traversal(int &ref_data)
+    {
 
-while(p){
-if(visitedNodes[p->destver]==false){
-s.push(p->destver);
-}
-p=p->next;
-}
-}
+        Node *traverse = &arr[0];
+        int i = 0;
+        while(traverse->data!=ref_data)
+        {
+            i++;
+            traverse = &arr[i];
+        }
+        if(traverse->data == ref_data)
+        {
+            if(!obj.find_data(ref_data))
+                cout<<traverse->data<<" ";
+            obj.insert_node(traverse->data);
+            while(traverse->next!=nullptr)
+            {
+                traverse = traverse->next;
+                if(!obj.find_data(traverse->data))
+                    DFS_traversal(traverse->data);
+            }
+        }
+    }
 
-
-}
-
-
-
+    void BFS_traversal(int &ref_data)
+    {
+        Node *traverse = &arr[0];
+        int i = 0;
+        while(traverse->data!=ref_data)
+        {
+            i++;
+            traverse = &arr[i];
+        }
+        Node *temp = traverse;
+        while(traverse!=nullptr)
+        {
+            if(!obj.find_data(traverse->data))
+            {
+                cout<<traverse->data<<" ";
+                obj.insert_node(traverse->data);
+            }
+            traverse = traverse->next;
+        }
+        traverse = temp;
+        obj2.insert_node(traverse->data);
+        while(traverse->next!=nullptr)
+        {
+            traverse=traverse->next;
+            if(!obj2.find_data(traverse->data))
+                BFS_traversal(traverse->data);
+        }
+    }
 };
 
-int main() {
+int main()
+{
+    Graph obj;
+    int choice, data;
+    while(choice!=5)
+    {
+        cout << "\n***********START*********";
+        cout << "\nMAIN MENU\n1. Create the graph.\n2. Display graph.\n3. DFS traversal.\n4. BFS traversal.\n5. Exit.\n";
+        cout << "Enter your choice:- ";
+        cin >> choice;
+        cout << endl;
+        switch (choice)
+        {
+        case 1:
+            obj.create_graph();
+            break;
 
-Graph a;
+        case 2:
+            obj.display();
+            break;
 
-int ch;
-while(ch!=5){
-cout<<"\n\n----------------MAIN MENU-----------------\n\n";
-cout<<"1) Insert Edges"<<endl;
-cout<<"2) Display Adjacency List"<<endl;
-cout<<"3) Display Graph using DFS"<<endl;
-cout<<"4) Display Graph using BFS"<<endl;
-cout<<"5) Exit"<<endl;
-cout<<"\n------------------------------------------\n";
+        case 3:
+            cout<<"Enter the start:- ";
+            cin>>data;
+            obj.DFS_traversal(data);
+            obj.obj.delete_SLL();
+            break;
 
-cout<<"\nEnter your choice -----> ";
-cin>>ch;
+        case 4:
+            cout<<"Enter the start:- ";
+            cin>>data;
+            obj.BFS_traversal(data);
+            obj.obj.delete_SLL();
+            obj.obj2.delete_SLL();
+            break;
 
-switch(ch){
-case 1:
-a.insert_edges();
-break;
-case 2:
-a.display();
-break;
-case 3:
-a.disp_dfs();
-break;
-case 4:
-a.disp_bfs();
-break;
-case 5:
-cout<<"\nThank you!!!!!";
-break;
+        case 5:
+            cout<<"Thank you!";
+            break;
+
+        default:
+            cout<<"Please enter a valid choice!";
+            break;
+        }
+    }
+    return 0;
 }
-}
-
-return 0;
-}
-
