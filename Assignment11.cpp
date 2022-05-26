7eaf506bbbac203d760a43f34bc35342e18d9760
Nikita Gokhale <81181608+nikitagokhale@users.noreply.github.com>
2022-05-26 22:13:14 +0530

    Update Assignment11.cpp

--- Assignment11.cpp
+++ Assignment11.cpp
@@ -1,82 +1,97 @@
-//============================================================================
-// Name        : Assignment_Heap_sort.cpp
-// Author      :
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
-#include <iostream>
-
+//Heap sort
+#include<iostream>
+#define SIZE 30
 using namespace std;
 
-// To heapify a subtree rooted with node i which is
-// an index in arr[]. n is size of heap
-void heapify(int arr[], int n, int i)
+class Heap_sort
 {
-    int largest = i; // Initialize largest as root
-    int l = 2 * i + 1; // left = 2*i + 1
-    int r = 2 * i + 2; // right = 2*i + 2
+    int heap[SIZE];
 
-    // If left child is larger than root
-    if (l < n && arr[l] > arr[largest])
-        largest = l;
-
-    // If right child is larger than largest so far
-    if (r < n && arr[r] > arr[largest])
-        largest = r;
-
-    // If largest is not root
-    if (largest != i) {
-        swap(arr[i], arr[largest]);
-
-        // Recursively heapify the affected sub-tree
-        heapify(arr, n, largest);
+public:
+    void create()
+    {
+        int data, n;
+        cout<<"Enter the number of elements:- ";
+        cin>>n;
+        cout<<"Enter data:- "<<endl;
+        heap[0]=0;
+        for(int i=0;i<n;i++)
+        {
+            cout<<">";
+            cin>>data;
+            insert_element(heap, data);
+        }
     }
-}
 
-// main function to do heap sort
-void heapSort(int arr[], int n)
-{
-    // Build heap (rearrange array)
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
-
-    // One by one extract an element from heap
-    for (int i = n - 1; i > 0; i--) {
-        // Move current root to end
-        swap(arr[0], arr[i]);
-
-        // call max heapify on the reduced heap
-        heapify(arr, i, 0);
+    void insert_element(int heap[], int data)
+    {
+        int n=heap[0];
+        heap[n+1]=data;
+        heap[0]=n+1;
+        upAdjust(heap, n+1);
     }
-}
 
-/* A utility function to print array of size n */
-void printArray(int arr[], int n)
-{
-    for (int i = 0; i < n; ++i)
-        cout << arr[i] << " ";
-    cout << "\n";
-}
+    void upAdjust(int heap[], int i)
+    {
+        int temp;
+        while(i>1 && heap[i]>heap[i/2])
+        {
+            temp = heap[i/2];
+            heap[i/2]=heap[i];
+            heap[i]=temp;
+            i = i/2;
+        }
+    }
 
-// Driver code
+    void display_heap()
+    {
+        cout<<"Number of elements:- "<<heap[0]<<endl;
+        for(int i=1;i<=heap[0];i++)
+            cout<<heap[i]<<" ";
+    }
+
+    void heap_sort()
+    {
+        int n=heap[0], top=heap[0];
+        for(int i=0;i<n-1;i++)
+        {
+            int temp = heap[top];
+            heap[top] = heap[1];
+            heap[1] = temp;
+            top--;
+            downAdjust(heap, top, 1);
+        }
+    }
+
+    void downAdjust(int heap[], int n, int i)
+    {
+        int flag = 1, j;
+        while(2*i<=n && flag)
+        {
+            j=i*2;
+            if(j+1<=n && heap[j]<heap[j+1])
+                j++;
+            if(heap[i]>heap[j])
+                flag = 0;
+            else
+            {
+                int temp=heap[j];
+                heap[j]=heap[i];
+                heap[i]=temp;
+                i=j;
+            }
+        }
+    }
+};
+
 int main()
 {
-//    int arr[] = {7,9,13,1,33,31,10};
-
-cout<<"Enter the size of array: ";
-    int n;
-    cin>>n;
-
-    int arr[n];
-    for(int i=0;i<n;i++){
-    cout<<"Enter the element at index "<<i<<" : ";
-    cin>>arr[i];
-    }
-
-    heapSort(arr, n);
-
-    cout << "Sorted array is \n";
-    printArray(arr, n);
+    Heap_sort obj;
+    obj.create();
+    cout<<"Before sorting!\n";
+    obj.display_heap();
+    obj.heap_sort();
+    cout<<"\nAfter sorting!\n";
+    obj.display_heap();
+    return 0;
 }
